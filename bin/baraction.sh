58562f2c4758e3071e6adee0b2183ec71c059e3f
Benjamin Althues <benjamin@babab.nl>
2015-08-25 08:10:32 +0200

    No more conky, just use plain old bin/baraction.sh

--- bin/baraction.sh
+++ bin/baraction.sh
@@ -8,9 +8,9 @@ while :; do
     let MINUTES="$COUNT/6"
     echo -n "$(uname -r) || "
     echo -n "$(cat /proc/loadavg | awk '{ print $1" "$2" "$3 }') || "
-    echo -n "$(sensors | grep temp1 | awk '{ print $2 }') "
-    echo -n "$(sensors | grep Core0 | awk '{ print $3 }') "
-    echo -n "$(sensors | grep Core1 | awk '{ print $3 }') || "
+    # echo -n "$(sensors | grep temp1 | awk '{ print $2 }') "
+    # echo -n "$(sensors | grep Core0 | awk '{ print $3 }') "
+    # echo -n "$(sensors | grep Core1 | awk '{ print $3 }') || "
     echo "${MINUTES}m"
     sleep $SLEEP_SEC
 done
