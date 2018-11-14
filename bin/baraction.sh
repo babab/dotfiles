ca62ca5bcb272d34892dc20e593629ab151815a4
Benjamin Althues <benjamin@babab.nl>
2018-11-14 20:24:06 +0100

    Add battery info to baraction script

--- bin/baraction.sh
+++ bin/baraction.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 # baraction.sh script for spectrwm status bar
 
+battinfo()
+{
+    out="$(acpi -b | sed -e 's/^Battery 0: //' -e 's/, / | /g')"
+
+    case $out in
+    Discharging*)
+        echo -n "BATT | "
+        echo "$out" | sed 's/^.*Discharging | //'
+        ;;
+    Charging*)
+        echo -n "ONLINE | "
+        echo "$out" | sed 's/^.*Charging | //'
+        ;;
+    *)
+        echo "$out"
+        ;;
+    esac
+}
+
 SLEEP_SEC=10
 COUNT=0
 while :; do
     let COUNT=$COUNT+1
     let MINUTES="$COUNT/6"
-    echo -n "$(uname -r) || "
-    echo -n "$(cat /proc/loadavg | awk '{ print $1" "$2" "$3 }') || "
-    # echo -n "$(sensors | grep temp1 | awk '{ print $2 }') "
-    # echo -n "$(sensors | grep Core0 | awk '{ print $3 }') "
-    # echo -n "$(sensors | grep Core1 | awk '{ print $3 }') || "
-    echo "${MINUTES}m"
+    echo -n "$(uname -r)   "
+    echo "${MINUTES}m   "
+    echo "$(battinfo)   "
     sleep $SLEEP_SEC
 done
