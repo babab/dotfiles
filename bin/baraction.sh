f550e3fbb4788ab27288f2e4072668a5f45cf069
Benjamin Althues <benjamin@babab.nl>
2023-03-02 01:11:43 +0100

    Make bin/baraction.sh POSIX-compliant

--- bin/baraction.sh
+++ bin/baraction.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 # baraction.sh script for spectrwm status bar
 
-battinfo()
-{
+battinfo() {
     out="$(acpi -b | sed -e 's/^Battery 0: //' -e 's/, / | /g')"
 
     case $out in
     Discharging*)
-        echo -n "BATT | "
+        printf "BATT | "
         echo "$out" | sed 's/^.*Discharging | //'
         ;;
     Charging*)
-        echo -n "CHARGING | "
+        printf "CHARGING | "
         echo "$out" | sed 's/^.*Charging | //'
         ;;
     Unknown*)
@@ -23,16 +22,15 @@ battinfo()
     esac
 }
 
-ipaddress()
-{
-    echo $(ip addr | grep 'inet ' | tail -1 | sed -e 's/inet //' -e 's/\/24.*//')
+ipaddress() {
+    ip addr | grep 'inet ' | tail -1 | sed -e 's/inet //' -e 's/\/24.*//'
 }
 
 SLEEP_SEC=30
 COUNT=0
 while :; do
-    let COUNT=$COUNT+1
-    let MINUTES="$COUNT/2"
+    COUNT=$((COUNT+1))
+    MINUTES=$((COUNT/2))
     echo "${MINUTES}m | $(battinfo) | $(ipaddress)"
     sleep $SLEEP_SEC
 done
